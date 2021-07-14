5e7d7514b25b52b7693b5f7e2c788839a203e6ad
Hiroshi Takaichi <hirosi.t822@gmail.com>
2021-07-15 00:19:26 +0900

    add

---
+++ GitHub/the-highest-building.py
@@ -0,0 +1,63 @@
+def highest_building(buildings):
+    buiiding_dict = {}
+    for building in buildings:
+        for i, cell in enumerate(building, 1):
+            if i not in buiiding_dict:
+                buiiding_dict[i] = 0
+            if cell == 1:
+                buiiding_dict[i] += 1
+
+    result = []
+    for k, v in buiiding_dict.items():
+        if not result or v > result[-1]:
+            result = [k, v]
+
+    return result
+
+    # good answer
+    # ポイント
+    # zip(*buildings)でlistを展開してタプルで各リストの同じインデックス
+    # を取得するタプルをつくっている
+    # lst = [sum(building) for building in zip(*buildings)]
+    # biggest = max(lst)
+    # return [lst.index(biggest)+1, biggest]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]) == [3, 4], "Common"
+    assert highest_building([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1]
+    ]) == [4, 1], "Cabin in the wood"
+    assert highest_building([
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1]
+    ]) == [1, 5], "Triangle"
+    assert highest_building([
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1]
+    ]) == [4, 6], "Pyramid"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
