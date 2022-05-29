508ede7a0b09e67f262fbdafaf3757279a37f0fc
Hiroshi Takaichi <hirosi.t822@gmail.com>
2022-05-29 17:45:44 +0900

    add

---
+++ version_2/scientific-expedition/time-converter-24h-to-12h.py
@@ -0,0 +1,22 @@
+def time_converter(time):
+    hour, minute = time.split(":")
+    new_hour = int(hour)
+    meridiem = "a.m."
+    if new_hour > 12:
+        new_hour = new_hour - 12
+        meridiem = "p.m."
+    elif new_hour == 12:
+        meridiem = "p.m."
+
+    return f"{new_hour}:{minute} {meridiem}"
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30'))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30') == '12:30 p.m.'
+    assert time_converter('09:00') == '9:00 a.m.'
+    assert time_converter('23:15') == '11:15 p.m.'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
